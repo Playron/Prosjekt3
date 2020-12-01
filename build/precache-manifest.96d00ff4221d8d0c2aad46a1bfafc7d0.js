9ce34ee26b5d177282889286aed3037407ecae63
Playron <pdhaagensen@gmail.com>
2020-12-01 23:16:09 +0100

    yay

---
+++ build/precache-manifest.96d00ff4221d8d0c2aad46a1bfafc7d0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2ef04086382d49ad74c799e75044ea2",
+    "url": "/Prosjekt3/index.html"
+  },
+  {
+    "revision": "39f2e2394ab65bcb1aba",
+    "url": "/Prosjekt3/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "7c2d0a9c8cd81d6646d6",
+    "url": "/Prosjekt3/static/css/main.7f7c61ab.chunk.css"
+  },
+  {
+    "revision": "39f2e2394ab65bcb1aba",
+    "url": "/Prosjekt3/static/js/2.de7b5080.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/Prosjekt3/static/js/2.de7b5080.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c2d0a9c8cd81d6646d6",
+    "url": "/Prosjekt3/static/js/main.ff461627.chunk.js"
+  },
+  {
+    "revision": "17f35e3c5ec486de4f84",
+    "url": "/Prosjekt3/static/js/runtime-main.f6a1e50f.js"
+  },
+  {
+    "revision": "f40034fe71c7bc1836525f0371c4251e",
+    "url": "/Prosjekt3/static/media/card.f40034fe.png"
+  }
+]);
